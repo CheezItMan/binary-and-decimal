3f9b68dc24ef9d4de80ff5cdd8badc3bedb8026c
Chris M <chris@adadevelopersacademy.org>
2020-09-16 13:06:45 -0700

    Update binary_to_decimal.rb

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,5 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  puts "answer"
 end
